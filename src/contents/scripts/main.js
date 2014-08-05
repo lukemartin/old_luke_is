95aa22c4bc8a278402ec1b0741e51ab542123325
Luke Martin <luke@lukemartin.me>
2014-08-05 12:32:03 +0100

    Updated project file

--- src/contents/scripts/main.js
+++ src/contents/scripts/main.js
@@ -2,3 +2,5 @@
 
 
 }).call(this);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uY29mZmVlIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFhO0FBQUE7QUFBQSIsImZpbGUiOiJtYWluLmpzIiwic291cmNlUm9vdCI6Ii9zb3VyY2UvIiwic291cmNlc0NvbnRlbnQiOlsiIl19
